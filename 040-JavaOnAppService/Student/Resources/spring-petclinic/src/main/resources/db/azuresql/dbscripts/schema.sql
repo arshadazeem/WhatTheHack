9b5575c532010cbd4ef966bb85220c13e9a48c38
arshadazeem <arshadazeem@gmail.com>
2022-01-18 20:24:21 -0500

    updated schema.sql with CREATE IF NOT EXISTS

--- 040-JavaOnAppService/Student/Resources/spring-petclinic/src/main/resources/db/azuresql/dbscripts/schema.sql
+++ 040-JavaOnAppService/Student/Resources/spring-petclinic/src/main/resources/db/azuresql/dbscripts/schema.sql
@@ -1,56 +1,86 @@
+-- vets
+IF NOT EXISTS (SELECT 0 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'vets')
+ BEGIN
+	  CREATE TABLE vets (
+	  id INT NOT NULL IDENTITY PRIMARY KEY,
+	  first_name VARCHAR(30),
+	  last_name VARCHAR(30),
+	  INDEX ix_vets_lname (last_name)
+	  );
+ END
 
-CREATE TABLE vets (
-  id INT NOT NULL IDENTITY PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  INDEX ix_vets_lname (last_name)
-);
+
+-- specialties
+IF NOT EXISTS (SELECT 0 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'specialties')
+ BEGIN
+	CREATE TABLE specialties (
+	  id INT NOT NULL IDENTITY PRIMARY KEY,
+	  name VARCHAR(80)
+	);
+END
 
 
-CREATE TABLE specialties (
-  id INT NOT NULL IDENTITY PRIMARY KEY,
-  name VARCHAR(80)
-);
+-- vet_specialties
+IF NOT EXISTS (SELECT 0 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'vet_specialties')
+ BEGIN           
+	CREATE TABLE vet_specialties (
+	  vet_id INT NOT NULL,
+	  specialty_id INT NOT NULL,
+	  FOREIGN KEY (vet_id) REFERENCES vets(id),
+	  FOREIGN KEY (specialty_id) REFERENCES specialties(id),
+	  UNIQUE (vet_id,specialty_id)
+	);
+ END
 
-CREATE TABLE vet_specialties (
-  vet_id INT NOT NULL,
-  specialty_id INT NOT NULL,
-  FOREIGN KEY (vet_id) REFERENCES vets(id),
-  FOREIGN KEY (specialty_id) REFERENCES specialties(id),
-  UNIQUE (vet_id,specialty_id)
-);
 
-CREATE TABLE types (
-  id INT NOT NULL IDENTITY PRIMARY KEY,
-  name VARCHAR(80)  
-) ;
+-- types
+IF NOT EXISTS (SELECT 0 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'types')
+ BEGIN
+	CREATE TABLE types (
+	  id INT NOT NULL IDENTITY PRIMARY KEY,
+	  name VARCHAR(80)  
+	);
+ END
 
-CREATE TABLE owners (
-  id INT NOT NULL IDENTITY PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  address VARCHAR(255),
-  city VARCHAR(80),
-  telephone VARCHAR(20),
-  INDEX ix_owners_lname (last_name)
-) ;
+-- owners
+IF NOT EXISTS (SELECT 0 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'owners')
+ BEGIN
+	CREATE TABLE owners (
+	  id INT NOT NULL IDENTITY PRIMARY KEY,
+	  first_name VARCHAR(30),
+	  last_name VARCHAR(30),
+	  address VARCHAR(255),
+	  city VARCHAR(80),
+	  telephone VARCHAR(20),
+	  INDEX ix_owners_lname (last_name)
+	);
+END
 
-CREATE TABLE pets (
-  id INT NOT NULL IDENTITY PRIMARY KEY,
-  name VARCHAR(30),
-  birth_date DATE,
-  type_id INT NOT NULL,
-  owner_id INT NOT NULL,
-  INDEX ix_pets_lname (name),
-  FOREIGN KEY (owner_id) REFERENCES owners(id),
-  FOREIGN KEY (type_id) REFERENCES types(id)
-) ;
 
-CREATE TABLE visits (
-  id INT NOT NULL IDENTITY PRIMARY KEY,
-  pet_id INT NOT NULL,
-  visit_date DATE,
-  description VARCHAR(255),
-  FOREIGN KEY (pet_id) REFERENCES pets(id)
-) ;
+-- pets
+IF NOT EXISTS (SELECT 0 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'pets')
+ BEGIN
+	CREATE TABLE pets (
+	  id INT NOT NULL IDENTITY PRIMARY KEY,
+	  name VARCHAR(30),
+	  birth_date DATE,
+	  type_id INT NOT NULL,
+	  owner_id INT NOT NULL,
+	  INDEX ix_pets_lname (name),
+	  FOREIGN KEY (owner_id) REFERENCES owners(id),
+	  FOREIGN KEY (type_id) REFERENCES types(id)
+	);
+END
 
+
+-- visits
+IF NOT EXISTS (SELECT 0 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'visits')
+ BEGIN
+	CREATE TABLE visits (
+	  id INT NOT NULL IDENTITY PRIMARY KEY,
+	  pet_id INT NOT NULL,
+	  visit_date DATE,
+	  description VARCHAR(255),
+	  FOREIGN KEY (pet_id) REFERENCES pets(id)
+	);
+ END
